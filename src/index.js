3098c045fdb2e197aea19204fa064036461181f8
user.email <pintupn1397@gmail.com>
2024-02-14 19:18:24 +0530

    supabase initialise

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"
+import "./index.css";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { createClient } from "@supabase/supabase-js";
+const apiUrl = "https://qbcybijsurnlebeyrere.supabase.co";
+const apiKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFiY3liaWpzdXJubGViZXlyZXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU3MzA4MTQsImV4cCI6MjAyMTMwNjgxNH0.bCDsliFJxm3WrNz5-bz6cRMYfFyude1casRiLuKz8mo";
+
+const supabase = createClient(apiUrl, apiKey);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
   </React.StrictMode>
 );
 
