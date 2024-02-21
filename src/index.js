b3e8bc4c8cdd0fc8c18a22185d8f1db49abad03d
user.email <pintupn1397@gmail.com>
2024-02-21 12:28:22 +0530

    modified:main.yaml

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
 import reportWebVitals from "./reportWebVitals";
-
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { createClient } from "@supabase/supabase-js";
 
+ require('dotenv').config()
+console.log(process.env)
 const apiUrl = process.env.REACT_APP_SUPABASE_URL;
 
 
